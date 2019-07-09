0495b4522060e43c53e31fe9dbf5893a68039d17
Justin Leider <justin@leiders.org>
2019-07-09 17:13:30 -0400

    Pull in fp-ts modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/pipeable";
+
+pipe(
+  O.some("hello world"),
+  O.fold(() => console.log("failed"), x => console.log(`${x} FooBar`)));
+
